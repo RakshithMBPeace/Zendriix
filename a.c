2d146e357c4865d5be3e2b6b2d66c47f6e5ac89a
Rakshith MB <rakshith.mb@qwikcilver.intra>
2019-12-25 09:47:52 +0530

    resolved conflicts

--- a.c
+++ a.c
@@ -4,12 +4,18 @@ public void base(){
 
 }
 
+
 publoic void featureA(){
 
 }
 
 public voig featureB(){
 
+}
+
+
+public void featureA(featureA){
+sysout("this is featureA")
 }
 
 public static void main(String []args){
